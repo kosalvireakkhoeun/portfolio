f5af7e19765b9d3d0097a83d1a497004d6cf811e
Kosalvireak <virakvary@gmail.com>
2024-07-01 22:41:45 +0700

    add api key

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,24 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey:  process.env.VUE_APP_API_KEY,
-    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-    databaseURL: process.env.VUE_APP_DATABASE_URL ,
-    projectId:process.env.VUE_APP_PROJECT_ID  ,
-    storageBucket: process.env.VUE_APP_STORAGE_BUCKET ,
-    messagingSenderId:process.env.VUE_APP_MESSAGING_SENDER_ID ,
-    appId: process.env.VUE_APP_APP_ID ,
+    apiKey:  "AIzaSyA_r-ew2QjzQGEXVDVSLzr404QTLdRaK3g",
+    authDomain: "kosalvireak-khoeun.firebaseapp.com",
+    databaseURL: "https://kosalvireak-khoeun-default-rtdb.firebaseio.com",
+    projectId:"kosalvireak-khoeun" ,
+    storageBucket: "kosalvireak-khoeun.appspot.com" ,
+    messagingSenderId:" 250441279398",
+    appId: "1:250441279398:web:483f3787fd2d2f222c5167" ,
   };
+
+  // const firebaseConfig = {
+  //   apiKey:  process.env.VUE_APP_API_KEY,
+  //   authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  //   databaseURL: process.env.VUE_APP_DATABASE_URL ,
+  //   projectId:process.env.VUE_APP_PROJECT_ID  ,
+  //   storageBucket: process.env.VUE_APP_STORAGE_BUCKET ,
+  //   messagingSenderId:process.env.VUE_APP_MESSAGING_SENDER_ID ,
+  //   appId: process.env.VUE_APP_APP_ID ,
+  // };
 
 const app = initializeApp(firebaseConfig);
 export default app;
